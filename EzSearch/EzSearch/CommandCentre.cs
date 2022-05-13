b8b55070ae0d2664580d3d6a1627e6b7370700b1
geeyemgee <geeyemgee@gmail.com>
2022-05-14 04:03:32 +0530

    skeleton file for master controller class

---
+++ EzSearch/EzSearch/CommandCentre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzSearch
+{
+    //Currently only the Search command is required
+    internal class CommandCentre
+    {
+    }
+}
--- EzSearch/EzSearch/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ EzSearch/EzSearch/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -94,7 +94,7 @@ namespace EzSearch {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EzSearch;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EzSearch;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
